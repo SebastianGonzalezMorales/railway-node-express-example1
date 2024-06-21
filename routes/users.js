2fc2ff9c0917f222d612c95f18d0c6875d6a065f
SebastianGonzalezMorales <Sebastian.gonzalez@alumnos.uv.cl>
2024-06-21 17:43:58 -0400

    Corriendo email: unique

--- routes/users.js
+++ routes/users.js
@@ -8,26 +8,25 @@ router.get(`/`, (req,res) => {
 });
 
 router.post('/register', async (req, res)=>{
+    const { name, email, mobile, password, userType } = req.body;
     let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        mobile: req.body.mobile,
-        password: req.body.password,
-        userType: req.body.userType,
-    })
+        name,
+        email,
+        mobile,
+        password,
+        userType,
+    });
     console.log(req.body);
 
     try {
         user = await user.save();
-        if (!user) {
-            return res.status(400).send('The user cannot be created!');
-        }
-        res.send(user);
+        res.status(201).send(user);
     } catch (error) {
         if (error.code === 11000) {
             // Este código de error indica una violación del índice único
             res.status(400).send({ message: 'El correo electrónico ya está en uso' });
         } else {
+            console.error('Error al crear el usuario:', error);
             res.status(500).send({ message: 'Error del servidor', error });
         }
     }
